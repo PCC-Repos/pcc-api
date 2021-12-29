d50db6878ac006cf9b3784070e9c70d3465ea3fb
Sairam Kumar <61322406+sairam4123@users.noreply.github.com>
2021-12-29 23:34:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:1234@localhost:5858/first_class_api_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://zimvcqcqedddwv:7c4ac2e62f0124e525e603dd10968c2a76e2a9e1843ae4263947898b0e347dd4@ec2-52-71-231-180.compute-1.amazonaws.com:5432/d6o7k4ttukfg04"
 from db import db
 db.init_app(app)
 
