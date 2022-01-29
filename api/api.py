41789ee1df9ad140ce09309384f5f561f292940b
sairam4123 <kannanscoffee@outlook.com>
2022-01-30 00:35:44 +0530

    Implement transactions. * Add some partial fields.

--- api/api.py
+++ api/api.py
@@ -9,26 +9,73 @@ bp_api = Blueprint("api", __name__)
 api = Api(bp_api)
 ns_api = api.namespace("api", path="/api/v1")
 
-member_fields = api.model('ClubMember', {
-    'id': fields.String(),
-    'nick': fields.String(),
-    'joined_at': fields.DateTime()
-})
-
-user_fields = api.model('User', {
+partial_user_fields = api.model('PartialUser', {
     'id': fields.String(),
     'name': fields.String(),
     'discord_id': fields.String(),
     'money': fields.String(),
     'created_at': fields.DateTime(),
-    'guild_id': fields.String()
+    'guild_id': fields.String(),
+})
+
+member_fields = api.model('ClubMember', {
+    'nick': fields.String(),
+    'joined_at': fields.DateTime(),
+    'user': fields.Nested(partial_user_fields)
+})
+
+partial_member_fields = api.model('PartialClubMember', {
+    'id': fields.String(),
+    'nick': fields.String(),
+    'joined_at': fields.DateTime(),
 })
 
 club_fields = api.model('Club', {
     'id': fields.String(),
     'name': fields.String(),
     'description': fields.String(),
-    'members': fields.List(fields.Nested(member_fields))
+    'members': fields.List(fields.Nested(partial_member_fields))
+})
+
+partial_club_fields = api.model('PartialClub', {
+    'id': fields.String(),
+    'name': fields.String(),
+    'description': fields.String(),
+})
+user_fields = api.model('User', {
+    'id': fields.String(),
+    'name': fields.String(),
+    'discord_id': fields.String(),
+    'money': fields.String(),
+    'created_at': fields.DateTime(),
+    'guild_id': fields.String(),
+    'clubs': fields.List(fields.Nested(partial_club_fields))
+})
+
+item_fields = api.model('Item', {
+    'id': fields.String(),
+    'name': fields.String(),
+    'description': fields.String(),
+    'object_id': fields.String(),
+    'seller': fields.Nested(partial_user_fields),
+    'cost': fields.String(),
+    'object_type': fields.Integer()
+})
+partial_item = api.model('PartialItem', {
+    'id': fields.String(),
+    'name': fields.String(),
+    'description': fields.String(),
+    'object_id': fields.String(),
+    'cost': fields.String(),
+    'object_type': fields.Integer()
+})
+
+transaction_fields = api.model('Transaction', {
+    'id': fields.String(),
+    'buyer': fields.Nested(partial_user_fields),
+    'seller': fields.Nested(partial_user_fields),
+    'item': fields.Nested(partial_item),
+    'cost': fields.String(),
 })
 
 clubs_request_parser = RequestParser()
@@ -54,6 +101,25 @@ user_patch_parser.add_argument("money", type=str, location="json")
 
 member_patch_parser = RequestParser()
 member_patch_parser.add_argument("nick", type=str, location="json")
+
+item_patch_parser = RequestParser()
+item_patch_parser.add_argument("name", type=str, location="json", store_missing=False)
+item_patch_parser.add_argument("description", type=str, location="json", store_missing=False)
+item_patch_parser.add_argument("cost", type=int, location="json", store_missing=False)
+
+item_add_parser = RequestParser()
+item_add_parser.add_argument("object_id", type=int, location="json")
+item_add_parser.add_argument("object_type", type=int, location="json")
+item_add_parser.add_argument("cost", type=int, location="json")
+item_add_parser.add_argument("seller_id", type=int, location="json")
+item_add_parser.add_argument("name", type=str, location="json")
+item_add_parser.add_argument("description", type=str, location="json")
+
+transaction_create_parser = RequestParser()
+transaction_create_parser.add_argument("item_id", type=int, location="json")
+transaction_create_parser.add_argument("seller_id", type=int, location="json")
+transaction_create_parser.add_argument("buyer_id", type=int, location="json")
+# transaction_create_parser.add_argument("cost", type=int, location="json")
 
 
 @ns_api.route("/clubs/<int:club_id>", endpoint="club")  # noqa
@@ -101,13 +167,13 @@ class ClubsResource(Resource):
 @ns_api.route("/clubs/<int:club_id>/members", endpoint="members")  # noqa
 class ClubMembersResource(Resource):
     @staticmethod
-    @ns_api.marshal_list_with(member_fields)
+    @ns_api.marshal_list_with(member_fields, envelope="members")
     def get(club_id):
         return Club.query.filter_by(id=club_id).first_or_404().members
 
     @staticmethod
     @ns_api.expect(user_request_parser)
-    @ns_api.marshal_with(member_fields)
+    @ns_api.marshal_with(member_fields, envelope="member")
     def post(club_id):
         data = user_request_parser.parse_args(strict=True)
         user = User.query.filter_by(id=data['id']).first()
@@ -117,21 +183,18 @@ class ClubMembersResource(Resource):
         db.session.add(member)
         db.session.commit()
         return member, 201
-    # @staticmethod
-    # @ns_api.expect(member_fields)
-    #
 
 
 @ns_api.route("/clubs/<int:club_id>/members/<int:member_id>", endpoint="member")  # noqa
 class ClubMemberResource(Resource):
     @staticmethod
-    @ns_api.marshal_with(member_fields)
+    @ns_api.marshal_with(member_fields, envelope="member")
     def get(club_id, member_id):
         return ClubMember.query.filter_by(id=member_id, club_id=club_id).first_or_404()
 
     @staticmethod
     @ns_api.expect(member_patch_parser)
-    @ns_api.marshal_with(member_fields)
+    @ns_api.marshal_with(member_fields, envelope="member")
     def patch(club_id, member_id):
         data = member_patch_parser.parse_args()
         ClubMember.query.filter_by(id=member_id, club_id=club_id).update(data)
@@ -139,7 +202,7 @@ class ClubMemberResource(Resource):
         return ClubMember.query.filter_by(id=member_id, club_id=club_id).first_or_404()
 
     @staticmethod
-    def delete(club_id, member_id):
+    def delete(club_id, member_id, envelope="member"):
         member = ClubMember.query.filter_by(id=member_id, club_id=club_id).first_or_404()
         db.session.delete(member)
         db.session.commit()
@@ -149,13 +212,13 @@ class ClubMemberResource(Resource):
 @ns_api.route("/users/<int:user_id>", endpoint="user")  # noqa
 class UserResource(Resource):
     @staticmethod
-    @ns_api.marshal_with(user_fields)
+    @ns_api.marshal_with(user_fields, envelope="user")
     def get(user_id):
         return User.query.filter_by(id=user_id).first_or_404()
 
     @staticmethod
     @ns_api.expect(user_patch_parser)
-    @ns_api.marshal_with(user_fields)
+    @ns_api.marshal_with(user_fields, envelope="user")
     def patch(user_id):
         data = user_patch_parser.parse_args()
         User.query.filter_by(id=user_id).update(data)
@@ -163,7 +226,7 @@ class UserResource(Resource):
         return User.query.filter_by(id=user_id).first_or_404()
 
     @staticmethod
-    def delete(user_id):
+    def delete(user_id, envelope="user"):
         user = User.query.filter_by(id=user_id).first_or_404()
         db.session.delete(user)
         db.session.commit()
@@ -180,22 +243,81 @@ class UsersResource(Resource):
             new_user = User(data['name'], data['discord_id'], data['guild_id'])
             db.session.add(new_user)
             db.session.commit()
-            return ns_api.marshal(new_user, user_fields), 201
+            return ns_api.marshal(new_user, user_fields, envelope="user"), 201
         else:
             return {"message": "User already exists"}, 400
 
     @staticmethod
-    @ns_api.marshal_list_with(user_fields)
+    @ns_api.marshal_list_with(user_fields, envelope="users")
     def get():
         return User.query.all()
 
 
-@ns_api.route("/transfer/", endpoint="transfers")
-class Transfers(Resource):
+@ns_api.route("/marketplace/items/", endpoint="items")
+class MarketplaceItemsResource(Resource):
     @staticmethod
+    @ns_api.marshal_with(item_fields, as_list=True)
     def get():
-        pass
+        return Item.query.all()
 
     @staticmethod
+    @ns_api.expect(item_add_parser)
+    @ns_api.marshal_with(item_fields)
     def post():
-        pass
+        data = item_add_parser.parse_args(strict=True)
+        new_item = Item(data["name"], data["description"], data["cost"], data["object_id"], ObjectType(data["object_type"]), data["seller_id"])
+        db.session.add(new_item)
+        db.session.commit()
+        return new_item
+
+
+@ns_api.route("/marketplace/items/<int:item_id>", endpoint="item")  # noqa
+class MarketplaceItemResource(Resource):
+    @staticmethod
+    @ns_api.marshal_with(item_fields)
+    def get(item_id):
+        return Item.query.filter_by(id=item_id).first_or_404()
+
+    @staticmethod
+    @ns_api.expect(item_patch_parser)
+    @ns_api.marshal_with(item_fields)
+    def patch(item_id):
+        data = item_patch_parser.parse_args()
+        Item.query.filter_by(id=item_id).update(data)
+        db.session.commit()
+        return Item.query.filter_by(id=item_id).first_or_404()
+
+    @staticmethod
+    def delete(item_id):
+        new_item = Item.query.filter_by(id=item_id).first_or_404()
+        db.session.delete(new_item)
+        db.session.commit()
+        return {'message': "Success!"}
+
+
+@ns_api.route("/marketplace/transactions")
+class TransactionsResource(Resource):
+    @staticmethod
+    @ns_api.expect(transaction_create_parser)
+    @ns_api.marshal_with(transaction_fields)
+    def post():
+        data = transaction_create_parser.parse_args(strict=True)
+        new_transaction = Transaction(data["item_id"], data["seller_id"], data["buyer_id"])
+        db.session.add(new_transaction)
+        db.session.flush()
+        new_transaction.transact()
+        db.session.commit()
+        return new_transaction, 201
+
+    @staticmethod
+    @ns_api.marshal_with(transaction_fields, as_list=True)
+    def get():
+        return Transaction.query.all()
+
+
+@ns_api.route("/marketplace/transactions/<int:transaction_id>")
+class TransactionResource(Resource):
+    @staticmethod
+    @ns_api.marshal_with(transaction_fields)
+    def get(transaction_id):
+        return Transaction.query.filter_by(id=transaction_id).first_or_404()
--- api/models.py
+++ api/models.py
@@ -1,12 +1,21 @@
 import datetime
+from enum import Enum
 
-from sqlalchemy import ForeignKey, PrimaryKeyConstraint
+from sqlalchemy import ForeignKey
 from sqlalchemy.dialects.postgresql import BIGINT
+from sqlalchemy.ext.associationproxy import association_proxy
 
 from db import BaseModel, db
 from id_generator import IDGenerator
 
 id_generator = IDGenerator.create(1609459200, 1, 1)
+
+
+class ObjectType(Enum):
+    MEMBER = 1
+
+    def __int__(self):
+        return self.value
 
 
 class Club(BaseModel):
@@ -18,7 +27,7 @@ class Club(BaseModel):
     description = db.Column(db.String)
     money = db.Column(BIGINT, default=25000000)
     guild_id = db.Column(BIGINT, nullable=False)
-    members = db.relationship("ClubMember", uselist=True, backref="clubs", lazy="select")
+    members = db.relationship("ClubMember", back_populates="club", uselist=True, lazy="select", cascade="all, delete")
 
     # member_ids = db.Column(db.ARRAY(BIGINT), db.ARRAY(db.ForeignKey("club_members.id")))
 
@@ -26,7 +35,6 @@ class Club(BaseModel):
         self.name = name
         self.description = description
         self.guild_id = guild_id
-        print(self.id)
 
 
 class User(BaseModel):
@@ -37,6 +45,8 @@ class User(BaseModel):
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
     guild_id = db.Column(BIGINT)
     name = db.Column(db.String)
+    _clubs = db.relationship("ClubMember", uselist=True, back_populates="user", lazy="select", cascade="all, delete")
+    clubs = association_proxy("_clubs", "club")
 
     def __init__(self, name: str, discord_id: int, guild_id: int):
         self.name = name
@@ -46,12 +56,57 @@ class User(BaseModel):
 
 class ClubMember(BaseModel):
     __tablename__ = "club_members"
-    user = db.relationship("User")
-    id = db.Column(BIGINT, ForeignKey("users.id"), primary_key=True)
+    user = db.relationship("User", back_populates="_clubs")
+    id = db.Column(BIGINT, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
     nick = db.Column(db.String)
-    club_id = db.Column(BIGINT, ForeignKey("clubs.id"), primary_key=True)
+    club = db.relationship("Club", back_populates="members")
+    club_id = db.Column(BIGINT, ForeignKey("clubs.id", ondelete="CASCADE"), primary_key=True)
     joined_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    value = db.Column(BIGINT)
 
     def __init__(self, _id, club_id):
         self.id = _id
         self.club_id = club_id
+        self.value = 0
+
+
+class Item(BaseModel):
+    __tablename__ = "items"
+    id = db.Column(BIGINT, nullable=False, primary_key=True, default=id_generator.create_id)
+    cost = db.Column(BIGINT)
+    object_id = db.Column(BIGINT, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    object_type = db.Column(db.Enum(ObjectType), nullable=False)
+    seller_id = db.Column(BIGINT, ForeignKey("users.id"), nullable=False)
+    seller = db.relationship("User", lazy="select")
+    stock = db.Column(db.Integer, default=1)
+
+    def __init__(self, name: str, description: str, cost: int, object_id: int, object_type: ObjectType, seller_id: int):
+        self.name = name
+        self.description = description
+        self.cost = cost
+        self.object_id = object_id
+        self.object_type = object_type
+        self.seller_id = seller_id
+
+
+class Transaction(BaseModel):
+    __tablename__ = "transactions"
+    id = db.Column(BIGINT, nullable=False, primary_key=True, default=id_generator.create_id)
+    item_id = db.Column(BIGINT, ForeignKey("items.id"), nullable=False)
+    seller_id = db.Column(BIGINT, ForeignKey("users.id"), nullable=False)
+    buyer_id = db.Column(BIGINT, ForeignKey("users.id"), nullable=False)
+    seller = db.relationship("User", foreign_keys=[seller_id])
+    buyer = db.relationship("User", foreign_keys=[buyer_id])
+    item = db.relationship("Item", foreign_keys=[item_id])
+
+    def __init__(self, item_id: int, seller_id: int, buyer_id: int):
+        self.item_id = item_id
+        self.seller_id = seller_id
+        self.buyer_id = buyer_id
+
+    def transact(self):
+        self.seller.money += self.item.cost
+        self.buyer.money -= self.item.cost
+        self.item.stock -= 1
